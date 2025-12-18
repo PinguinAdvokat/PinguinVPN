5ee310f96541f8059e9cee0579be09a53a6ca0c4
student <student.ksipt.ru>
2025-12-18 13:32:58 +0300

    z

--- payment.py
+++ payment.py
@@ -21,7 +21,7 @@ async def update_clients(bot: Bot):
                 if (not (history[i]['operation_id'], history[i]['label']) in bd_history) and history[i]['label']:
                     js = json.loads(str(history[i]['label']).replace("'", '"'))
                     storage.remove_promo(js["pr"], js["chat_id"])
-                    await storage.extend_user(js["chat_id"], int(js["months"]) * 30)
+                    await storage.extend_user(js["chat_id"], int(js["months"]))
                     storage.add_operations_history(history[i]['operation_id'], history[i]['label'])
                     await bot.send_message(js["chat_id"], f'Тариф успешно продлен на {js["months"]} месяц(ов), приятного пользования !\n(По всем вопросам обращайтесь в поддержку)')
             except KeyError:
